caebce0e353f330ec2bb72a2f58a393c9c4148fb
abner_davii <abnerdavigames@gmail.com>
2024-06-13 10:44:39 -0300

    Criando get e set

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,22 @@
-﻿namespace CardapioWeb.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CardapioWeb.Models
 {
     public class Produto
     {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo Obrigatório")]
+        [Display(Name ="Nome do Produto")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public  string Descricao_curta { get; set; }
+        public string Descricao_Detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public bool Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
     }
 }
